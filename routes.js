90607cbf20277388060902935a3dae7e7cd5dd9b
Juliana <aragaopintojuli@gmail.com>
2021-10-12 05:28:23 -0300

    more config

--- routes.js
+++ routes.js
@@ -3,13 +3,17 @@ import { states, alarm_logs } from './database/index.js';
 
 const routes = Router();
 
-routes.get('/states', async (req, res) => {
+routes.get('/api/states', async (req, res) => {
   const currentStates = await states();
+  res.setHeader('Content-Type', 'text/html');
+  res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
   return res.json(currentStates);
 });
 
-routes.get('/alarmlogs', async (req, res) => {
+routes.get('/api/alarmlogs', async (req, res) => {
   const alarmLogs = await alarm_logs();
+  res.setHeader('Content-Type', 'text/html');
+  res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
   return res.json(alarmLogs);
 });
 
