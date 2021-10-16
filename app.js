7f4977200cd8cfac99b1fae109a94533787032b1
Juliana <aragaopintojuli@gmail.com>
2021-10-16 13:37:01 -0300

    adjust on cors policy

--- app.js
+++ app.js
@@ -4,9 +4,8 @@ import cors from 'cors';
 
 const app = Express();
 app.use(Express.json());
-app.use(routes);
-
 app.use(cors());
+app.use(routes);
 
 const PORT = process.env.PORT || 3030;
 app.listen(PORT, () => {
